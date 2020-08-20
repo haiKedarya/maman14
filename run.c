559fdc988f99a3eea889ab8b933ff4a3a5494b9f
shoustak <52932659+shoustak@users.noreply.github.com>
2020-08-20 14:46:56 +0300

    Update run.c

--- run.c
+++ run.c
@@ -21,6 +21,49 @@ void printData(){
    }
    printf("\n");
 }
+
+void printEntry(){
+   EntryNode *temp;
+   if (ehead == NULL) {
+      printf("List is empty.\n");
+      return;
+   }
+      printf("========================= \n");
+   printf("elements of ENTRIES list are : \n");
+         printf("========================= \n");
+   temp = ehead;
+   while (temp != NULL) {
+      
+      printf("label name: %s\n", temp->entry.label);
+               printf("========================= \n");
+      
+      temp = temp->next;
+   }
+   printf("\n");
+}
+
+void printToCheck(){
+   DataNode *temp;
+   if (dhead == NULL) {
+      printf("List is empty.\n");
+      return;
+   }
+      printf("========================= \n");
+   printf("We need to make sure if they are exist!!!!: \n");
+         printf("========================= \n");
+   temp = dhead;
+   while (temp != NULL) {
+      
+      printf("name: %s\n", temp->data.word);
+      printf("ic: %d \n", temp->data.ic);
+      
+               printf("========================= \n");
+      
+      temp = temp->next;
+   }
+   printf("\n");
+}
+
 void runProg(char * fileName[]){
     {   
         fp = fopen(fileName, "r");
@@ -30,6 +73,8 @@ void runProg(char * fileName[]){
 
 	firstParse(fp);
 	printData();
+	printEntry();
+	printToCheck();
 /*
         fclose(fp);
         if (line){
