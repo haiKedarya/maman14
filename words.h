caec4baa6f3a3b1b0d5a52a1c3820dfd7dc1be7e
shoustak <52932659+shoustak@users.noreply.github.com>
2020-08-20 14:47:17 +0300

    Update words.h

--- words.h
+++ words.h
@@ -1,4 +1,3 @@
-char array [] = {"sdsad"}; 
 
 typedef struct int24{
     unsigned int data : 24;
@@ -11,12 +10,13 @@ static int24 * arr24 [100];
 int24 arrNum[100];
 
 
-void makeBinary(int toPrint){
+int makeBinary(int toPrint,int sibit){
 
 unsigned i; 
 
-    for (i = 1 << 24; i > 0; i = i / 2) 
+    for (i = 1 << sibit; i > 0; i = i / 2) 
         (toPrint & i)? printf("1"): printf("0"); 
-  
+  printf("\n");
+  return 1;
   
 }
